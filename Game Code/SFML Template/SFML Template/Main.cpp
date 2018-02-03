f594d2e45f689daff14cb146d3f7ac8e4350733d
Hoodrn03 <Hoodrn03@gmail.com>
2018-02-03 20:51:20 +0000

    Comments
    I have added some comments into both my main and defs files.

--- Game Code/SFML Template/SFML Template/Main.cpp
+++ Game Code/SFML Template/SFML Template/Main.cpp
@@ -1,28 +1,24 @@
 
-#include <SFML\Graphics.hpp>
-#include <Box2D\Box2D.h>
+/*!\mainpage Ball Fall (Object Oriented Design and Development Assignment 2)
+*
+*This program will contain the completed game will full functionality. The game must be 
+*include both SFML and a physics system within the game, I will use the library Box2D 
+*for the physics within my game. 
+*
+*My game will be based off of the already exsisting game Cut the rope, and will feature 
+*similar mechanics. The player will have to cut ropes within the game using their mouve to
+*make their ball fall into the end point, whilst attempting to solve puzzles and avoiding 
+*obsticals. 
+*
+*/
 
+
+//! This will be used to gain access to all of the linked libraries which will be used everywhere in the program. 
+#include "defs.h" 
+
+
+//! main : The entry point for the program.  
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Game Window");
-
-	sf::Event event;
-
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == event.Closed)
-			{
-				window.close();
-			}
-		}
-
-		window.clear();
-
-		window.display();
-
-	}
-
 	return 0;
 }
---
+++ Game Code/SFML Template/SFML Template/defs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/*!\file defs.h
+*
+*This will be used to hold all of the vital includes which will span all of 
+*the project. All of the other files will be linked to this one. 
+*
+*/
+
+
+//! This will allow access to SFML's graphics features
+#include <SFML\Graphics.hpp>
+
+//! This will allow access to the physics system which i plan to use within my game.
+#include <Box2D\Box2D.h>
+
+//! This will allow me to access useful functions like cout and cin. 
+#include <iostream>
